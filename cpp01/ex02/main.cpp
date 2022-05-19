d22b039de8f468905abd3e1d7fa7ddb48b70f2e3
Freek Bes <freekbladnl@gmail.com>
2022-05-19 22:43:27 +0200

    ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fbes <fbes@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/19 22:23:05 by fbes          #+#    #+#                 */
+/*   Updated: 2022/05/19 22:42:20 by fbes          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int		main(void)
+{
+	std::string		string = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &string;
+	std::string&	stringREF = string;
+
+	std::cout << "Memory address of string:         " << &string << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of string:    " << string << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+}
