755f1370e1b92632d81411bb07218203f00aac1d
Freek Bes <freek.bes@codam.nl>
2022-09-01 17:10:05 +0200

    add basics for ex02

---
+++ cpp05/ex02/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fbes <fbes@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/05/23 17:47:19 by fbes          #+#    #+#                 */
+/*   Updated: 2022/09/01 17:05:45 by fbes          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int		main(void)
+{
+	Bureaucrat mayor("Mayor", 1);
+	Bureaucrat assistant("Assistant", 100);
+
+
+}
