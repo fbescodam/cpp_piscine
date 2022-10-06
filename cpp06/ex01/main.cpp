8641cd2407f5630d5cf521254d2ea787c724857c
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-10-06 15:26:29 +0200

    is this really it

---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fbes <fbes@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/06 15:17:16 by fbes          #+#    #+#                 */
+/*   Updated: 2022/10/06 15:26:01 by fbes          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "data.hpp"
+#include <iostream>
+
+uintptr_t serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data*>(raw));
+}
+
+int main(void)
+{
+	Data		data;
+	uintptr_t 	serialized;
+	Data*		deserialized;
+
+	data.content = 42;
+	std::cout << "Original pointer: " << &data << std::endl;
+	std::cout << "Original content: " << data.content << std::endl;
+
+	serialized = serialize(&data);
+	std::cout << "Serialized address: " << serialized << std::endl;
+
+	deserialized = deserialize(serialized);
+	std::cout << "Deserialized pointer: " << deserialized << std::endl;
+	std::cout << "Deserialized content: " << deserialized->content << std::endl;
+
+	return (0);
+}
