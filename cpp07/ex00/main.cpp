dc9d3165cb73926b7d2c4e5cb28bd72aac0c7fd4
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2022-11-03 18:29:00 +0100

    update main

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/03 18:07:41 by fbes          #+#    #+#                 */
-/*   Updated: 2022/11/03 18:27:23 by fbes          ########   odam.nl         */
+/*   Updated: 2022/11/03 18:28:22 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 int main(void)
 {
-	int a = 42;
-	int b = 10;
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
-	swap<int>(a, b);
-	std::cout << "swapped" << std::endl;
-	std::cout << "a: " << a << std::endl;
-	std::cout << "b: " << b << std::endl;
-	std::cout << "min: "<< min<int>(a, b) << std::endl;
-	std::cout << "max: "<< max<int>(a, b) << std::endl;
-	std::cout << "equal min: "<< min<int>(12, 12) << std::endl;
-	std::cout << "equal max: "<< max<int>(12, 12) << std::endl;
-	return (0);
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
