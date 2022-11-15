bd0da3f02040ed2f3ad96066d00092bee06748fa
Freek Bes <freek.bes@codam.nl>
2022-11-15 11:36:41 +0100

    return value of iterator

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/11 10:26:23 by fbes          #+#    #+#                 */
-/*   Updated: 2022/11/11 11:15:24 by fbes          ########   odam.nl         */
+/*   Updated: 2022/11/15 11:35:51 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int easyfind(const T& container, const int n)
 	typename T::const_iterator res = std::find(container.begin(), container.end(), n);
 	if (res == container.end())
 		throw std::exception();
-	return n;
+	return *res;
 }
